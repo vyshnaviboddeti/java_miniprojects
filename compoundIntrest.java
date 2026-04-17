e732ccbf7e7ddcc59a370b1c0e584d8d9d265da1
Vyshnavi24 <155798167+vyshnaviboddeti@users.noreply.github.com>
2026-04-17 22:19:51 +0530

    compound interest calculation using java

--- compoundIntrest.java
+++ compoundIntrest.java
@@ -15,7 +15,7 @@ public class compoundIntrest {
         rate = sc.nextDouble();
         System.out.println("enter no. of timesCompounded per year :");
         timesCompounded = sc.nextInt();
-        System.out.println("enter no of years:");
+        System.out.println("enter no of years :");
         years = sc.nextInt();
         amount = principal * Math.pow(1+rate/timesCompounded,timesCompounded*years);
         System.out.println("The amount after" + years + "is :" +amount);
